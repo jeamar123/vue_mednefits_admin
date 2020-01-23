23aa5fc45d8fc967d143ca848145a82942c70416
Jeamar Libres <jeamarlibres@Jeamars-MacBook-Air.local>
2020-01-23 16:04:38 +0800

    dupates

--- src/views/corporates/CorporateMenu.vue
+++ src/views/corporates/CorporateMenu.vue
@@ -4,10 +4,7 @@
 		<div class="left-box-wrapper">
 			<div class="corporate-details-box">
 				<router-link tag="a" :to="{ name: 'Corporates' }" class="close-btn"><span class="oi" data-glyph="x" aria-hidden="true"></span></router-link>
-				<img :src="'../assets/img/user-group.png'">
-				<p class="corporate-name">
-					Allan Company
-				</p>
+				<img :src="'../assets/img/admin_user.png'">
 
 				<div class="company-basic-details">
 					<div class="info-div">
@@ -27,49 +24,104 @@
 						<p>24</p>
 					</div>
 				</div>
-			</div>
 
-			<div class="details-menu-wrapper">
-				<div class="menu-item" v-bind:class="{ 'active' : corporateViewStatus == 'CorporateInformation' }" v-on:click="selectCorporateView( 'CorporateInformation' )">
-					<label>Information</label>
-					<span class="oi" data-glyph="caret-right" aria-hidden="true"></span>
+				<div class="company-basic-details enrollment-status">
+					<p class="info-title">Employee Enrollment Status</p>
+					
+					<div class="info-div-inline">
+						<label>Total seats:</label>
+						<p>104</p>
+					</div>
+					<div class="info-div-inline">
+						<label>Occupied Seats:</label>
+						<p>42</p>
+					</div>
+					<div class="info-div-inline">
+						<label>Vacant Seats:</label>
+						<p>62</p>
+					</div>
+
+					<div class="white-space-10"></div>
+					<p class="info-title">Dependent Enrollment Status</p>
+
+					<div class="info-div-inline">
+						<label>Total seats:</label>
+						<p>104</p>
+					</div>
+					<div class="info-div-inline">
+						<label>Occupied Seats:</label>
+						<p>42</p>
+					</div>
+					<div class="info-div-inline">
+						<label>Vacant Seats:</label>
+						<p>62</p>
+					</div>
+					<div class="white-space-10"></div>
 				</div>
-				<div class="menu-item" v-bind:class="{ 'active' : corporateViewStatus == 'CorporateMemberList' }" v-on:click="selectCorporateView( 'CorporateMemberList' )">
-					<label>Member List</label>
-					<span class="oi" data-glyph="caret-right" aria-hidden="true"></span>
-				</div>
-				<div class="menu-item" v-bind:class="{ 'active' : corporateViewStatus == 'CorporatePlan' }" v-on:click="selectCorporateView( 'CorporatePlan' )">
-					<label>Corporate Details</label>
-					<span class="oi" data-glyph="caret-right" aria-hidden="true"></span>
-				</div>
-				<div class="menu-item" v-bind:class="{ 'active' : corporateViewStatus == 'SpendingInvoice' }" v-on:click="selectCorporateView( 'SpendingInvoice' )">
-					<label>Spending Invoice</label>
-					<span class="oi" data-glyph="caret-right" aria-hidden="true"></span>
-				</div>
-				<div class="menu-item" v-bind:class="{ 'active' : corporateViewStatus == 'CorporateEnrollment' }" v-on:click="selectCorporateView( 'CorporateEnrollment' )">
-					<label>Enrollment</label>
-					<span class="oi" data-glyph="caret-right" aria-hidden="true"></span>
-				</div>
-				<div class="menu-item" v-bind:class="{ 'active' : corporateViewStatus == 'BulkCreditAllocation' }" v-on:click="selectCorporateView( 'BulkCreditAllocation' )">
-					<label>Bulk Credit Allocation</label>
-					<span class="oi" data-glyph="caret-right" aria-hidden="true"></span>
-				</div>
-				<div class="menu-item" v-bind:class="{ 'active' : corporateViewStatus == 'EclaimTypeService' }" v-on:click="selectCorporateView( 'EclaimTypeService' )">
-					<label>E-Claim - Claim Type Services</label>
-					<span class="oi" data-glyph="caret-right" aria-hidden="true"></span>
-				</div>
-				<div class="menu-item" v-bind:class="{ 'active' : corporateViewStatus == 'HealthPartnerAccess' }" v-on:click="selectCorporateView( 'HealthPartnerAccess' )">
-					<label>Health Partnner Access</label>
-					<span class="oi" data-glyph="caret-right" aria-hidden="true"></span>
-				</div>
-				<div class="menu-item" v-bind:class="{ 'active' : corporateViewStatus == 'GcapPerVisit' }" v-on:click="selectCorporateView( 'GcapPerVisit' )">
-					<label>GP - Cap per Visit</label>
-					<span class="oi" data-glyph="caret-right" aria-hidden="true"></span>
-				</div>
-				<div class="menu-item" v-bind:class="{ 'active' : corporateViewStatus == 'CorporateSettings' }" v-on:click="selectCorporateView( 'CorporateSettings' )">
-					<label>Settings</label>
-					<span class="oi" data-glyph="caret-right" aria-hidden="true"></span>
-				</div>
+
+				<div class="company-basic-details company-credits">
+					<p class="info-title">Company Credits:</p>
+					<p class="info-title">Medical Spending Account:</p>
+
+					<div class="info-div-inline">
+						<label>Total:</label>
+						<p><span>SGD</span> 74,985.08</p>
+					</div>
+					<div class="info-div-inline">
+						<label>Allocated:</label>
+						<p><span>SGD</span> 186,637.72</p>
+					</div>
+					<div class="info-div-inline">
+						<label>Unallocated:</label>
+						<p><span>SGD</span> -111,652.64</p>
+					</div>
+					<div class="info-div-inline">
+						<label>Spent:</label>
+						<p><span>SGD</span> 87,039.98</p>
+					</div>
+					<div class="info-div-inline">
+						<label>Balance:</label>
+						<p><span>SGD</span> 100,122.72</p>
+					</div>
+
+					<div class="white-space-20"></div>
+					<p class="info-title">Company Credits:</p>
+					<p class="info-title">Wellness Spending Account:</p>
+
+					<div class="info-div-inline">
+						<label>Total:</label>
+						<p><span>SGD</span> 74,985.08</p>
+					</div>
+					<div class="info-div-inline">
+						<label>Allocated:</label>
+						<p><span>SGD</span> 186,637.72</p>
+					</div>
+					<div class="info-div-inline">
+						<label>Unallocated:</label>
+						<p><span>SGD</span> -111,652.64</p>
+					</div>
+					<div class="info-div-inline">
+						<label>Spent:</label>
+						<p><span>SGD</span> 87,039.98</p>
+					</div>
+					<div class="info-div-inline">
+						<label>Balance:</label>
+						<p><span>SGD</span> 100,122.72</p>
+					</div>
+
+					<div class="white-space-20"></div>
+					<div class="info-div credit-reset">
+						<label>Latest Credit Reset Date</label>
+						<p>02 January, 2020</p>
+					</div>
+
+					<div class="white-space-50"></div>
+					<button class="btn-primary txt-transform-uppercase">Plan Renewal Details</button>
+					<button class="btn-primary txt-transform-uppercase">Spending Account Renewal Details</button>
+					<button class="btn-primary txt-transform-uppercase">Send Plan Expiration Notification</button>
+					<button class="btn-primary txt-transform-uppercase">Corporate Details</button>
+					<button class="btn-primary txt-transform-uppercase">Settings</button>
+				</div>	
 			</div>
 		</div>
 
