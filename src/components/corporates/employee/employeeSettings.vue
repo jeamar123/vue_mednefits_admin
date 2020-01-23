8959dc9bf50ab0068edfceb580f3cbf773e9e5ce
Jeamar Libres <jeamarlibres@Jeamars-MacBook-Air.local>
2020-01-23 18:54:22 +0800

    restructure

---
+++ src/components/corporates/employee/employeeSettings.vue
@@ -0,0 +1,276 @@
+<script>
+import Modal from "../../../views/modal/Modal.vue";
+
+
+  let employeeSettings = {
+    components: {
+      Modal
+    },
+    data() {
+      return {
+        empSelectorActive: {
+          value: 0,
+          text: ""
+        },
+        editEmployeeProfile: false,
+        addDependentInfo: false,
+        editRemoveEmpInfo: false,
+        remove_step_active: 'remove-emp',
+        removeBackBtn: false,
+        editReplaceDependentInfo: false,
+        withdrawEmployeeModal: false,
+        inNetworkClaimSummaryModal: false,
+        editDependentInfo: false,
+        showInNetwork: false,
+        showOutNetwork: false,
+        spendingTypeOpt: 'medical',
+        step_active: 1,
+        showTimeVisitDropdown: false,
+        showDaytimeOption: false,
+        showTimeOption: false,
+        showSetupAccountModal: false,
+        showClaimTypeListOption: false,
+        showMemberListOption: false,
+        showRenewModal: false,
+        showManageCapPerVisit: false,
+        showSmsUpdateNotify: false,
+        selected_emp_details_opt: 0,
+        editCreditAllocationOpt: 'medical',
+        editCreditAllocationTypeOpt: 'add',
+        showEmpCreditsPlan: false,
+        starDateDetails: {
+          startDate: undefined,
+        },
+        showShortTermSelector: false,
+      };
+    },
+    created(){
+      this.healthPartnerViewStatus = this.$route.name;
+    },
+    methods: {
+      selectHealthPartnerView( opt ){
+        this.healthPartnerViewStatus = opt;
+        this.$router.push( { name : opt } );
+      },
+      showEmpSelectorInfo(value, text) {
+        this.empSelectorActive.value = value;
+        this.empSelectorActive.text = text;
+      },
+      showEditEmp() {
+        this.editEmployeeProfile = this.editEmployeeProfile == false ? true : false;
+      },
+      showAddDependent() {
+        this.addDependentInfo = this.addDependentInfo == false ? true : false;
+      },
+      showRemoveEmp() {
+        this.editRemoveEmpInfo = this.editRemoveEmpInfo == false ? true : false; 
+      },
+      showReplaceDependent() {
+        this.editReplaceDependentInfo = this.editReplaceDependentInfo == false ? true : false;
+      },
+      showRemoveDependent() {
+        this.withdrawEmployeeModal = this.withdrawEmployeeModal == false ? true : false;
+      },
+      showEditDependent() {
+        this.editDependentInfo = this.editDependentInfo == false ? true : false;
+      },
+      removeEmployeeBtn( data ) {
+        let x = data;
+
+        if(x === "back") {
+          if (this.remove_step_active == 'remove-opt' ) {
+            this.removeBackBtn = false;
+            this.remove_step_active = 'remove-emp';
+          }
+
+          if (this.remove_step_active == 'replace-emp') {
+            this.remove_step_active = 'remove-opt';
+          }
+
+          if (this.remove_step_active == 'health-spending-summary') {
+            this.remove_step_active = 'remove-opt';
+          }
+
+          if (this.remove_step_active == 'health-spending-account') {
+            this.remove_step_active = 'health-spending-summary';
+          }
+        }
+
+        if(x === "next") {
+         
+          if (this.remove_step_active == 'remove-emp' ) {
+            this.removeBackBtn = true;
+            this.remove_step_active = 'remove-opt';
+
+          } else if (this.remove_step_active == 'remove-opt') {
+
+            if ( this.emp_details_replace ) {
+              this.remove_step_active = 'replace-emp';          
+            } 
+
+            if ( this.emp_details_reserve ) {
+              this.remove_step_active = 'health-spending-summary';
+            }
+
+            if ( this.emp_details_remove ) {
+              this.remove_step_active = 'health-spending-summary';
+            }
+              
+          } else if (this.remove_step_active == 'replace-emp') {
+            this.remove_step_active = 'health-spending-summary';  
+
+          } else if (this.remove_step_active == 'health-spending-summary') {
+            this.remove_step_active = 'health-spending-account';
+          }
+            
+        }
+
+        
+      },
+      changeRemoveOption ( opt ) {
+        this.emp_details_replace = false;
+        this.emp_details_reserve = false;
+        this.emp_details_remove = false;
+
+        if ( opt === 1 ) {
+          this.emp_details_replace = true;
+        }
+        if ( opt === 2 ) {
+          this.emp_details_reserve = true;
+          console.log('2 ni siya');
+        }
+        if ( opt === 3 ) {
+          this.emp_details_remove = true;
+          console.log('3 ni siya');
+        }
+      },
+      inNetworkSubmit() { 
+        this.inNetworkClaimSummaryModal = this.inNetworkClaimSummaryModal == false ? true : false;
+      },
+      toggleShowInNetwork(data) {
+        let x = data;
+        if ( x === "in-network" ) {
+          this.showInNetwork = true;
+        } else if ( x === "cancel" ) {
+          this.showInNetwork = false;
+        }
+      },
+      toggleShowOutNetwork(data) {
+        let x = data;
+        if ( x === "out-network" ) {
+          this.showOutNetwork = true;
+        } else if ( x === "cancel" ) {
+          this.showOutNetwork = false;
+        }
+      },
+      editInNetworkOpt() {
+        this.showOutNetwork = true;
+      },
+      claimTypeListOption() {
+        this.showClaimTypeListOption = this.showClaimTypeListOption == false ? true : false;
+      },
+      memberListOption() {
+        this.showMemberListOption = this.showMemberListOption == false ? true : false;
+      },
+      setSpendingType(opt) {
+        this.spendingTypeOpt = opt;
+      },
+      clickedTimeVisitDropdown() {
+        this.showTimeVisitDropdown = this.showTimeVisitDropdown == false ? true : false;
+      },
+      clickedTimeOption() {
+        this.showTimeOption = this.showTimeOption == false ? true : false;
+      },
+      clickedDaytimeOption() {
+        this.showDaytimeOption = this.showDaytimeOption == false ? true : false;
+      },
+      empDetailsOutNetworkNextBackBtn( data ) {
+        let x = data;
+        if (x === "next") {
+          if (this.step_active == 1) {
+            this.step_active = 2;
+          } else if (this.step_active == 2) {
+            this.step_active = 3;
+          }
+        }
+        if (x === "back") {
+          if (this.step_active == 2 ) {
+            this.step_active = 1;
+          } else if (this.step_active == 3) {
+            this.step_active = 2;
+          }
+        }
+      },
+      //SETTINGS BUTTON FOR SHOWING MODAL
+      selectedEmpDetailsSettingsClicked(value,data) {
+        let y = value;
+        let x = data;
+
+        if ( y == 0 ) {
+          if ( x === "setup-reset-account" ) {
+            this.showSetupAccountModal = true;
+          } else if ( x === "cancel" ) {
+            this.showSetupAccountModal = false;
+          }
+        }
+        
+        if ( y == 1 ) {
+          if ( x === "renew-plan" ) {
+            this.showRenewModal = true;
+          } else if ( x === "cancel" ) {
+            this.showRenewModal = false;
+          }
+        }
+
+        if ( y == 2 ) {
+          if ( x === "manage-visit" ) {
+            this.showManageCapPerVisit = true;
+          } else if ( x === "cancel" ) {
+            this.showManageCapPerVisit = false;
+          }
+        }
+
+        if ( y == 3 ) {
+          if ( x === "sms-update-notify" ) {
+            this.showSmsUpdateNotify = true;
+          } else if ( x === "cancel" ) {
+            this.showSmsUpdateNotify = false;
+          }
+        }
+
+      },
+      selectedEmpDetailsToggleOpt(opt) {
+        this.selected_emp_details_opt = opt;
+      },
+      creditAllocationOption(opt) {
+        this.editCreditAllocationOpt = opt;
+      },
+      creditAllocationTypeFunction(opt) {
+        this.editCreditAllocationTypeOpt = opt;
+      },
+      openEmpCreditsPlan( data ) {
+        let x = data;
+        if ( x === "credits-plans" ) {
+          this.showEmpCreditsPlan = true;
+        } else if ( x === "cancel" ) {
+          this.showEmpCreditsPlan = false;
+        }
+      },
+      shortTermRadioBtn(data) {
+        let x = data;
+        if ( x === "short-term" ) {
+          this.showShortTermSelector = true;
+        } 
+        else if ( x === "standard-one-year") {
+          this.showShortTermSelector = false;
+        }
+      }
+    }
+  }
+  
+  export default employeeSettings
+</script>
+
+<style lang="scss" scoped>
+	@import "./src/assets/css/corporates.scss";
+</style>
---
+++ src/views/corporates/employee/EmployeeSettings.vue
@@ -0,0 +1,119 @@
+<template>
+	<div class="settings-details">
+		<div v-if="!showEmpCreditsPlan">
+			<div>
+				<button @click="selectedEmpDetailsSettingsClicked(0,'setup-reset-account')" class="btn btn-settings"><i class="fa fa-wrench"></i> Setup/Reset Account</button>
+			</div>
+			<div>
+				<button @click="openEmpCreditsPlan('credits-plans')" class="btn btn-settings"><i class="fa fa-check"></i> Credits & Plans</button>
+			</div>
+			<div>
+				<button @click="selectedEmpDetailsSettingsClicked(1,'renew-plan')" class="btn btn-settings"><i class="fa fa-refresh"></i> Re-new Plan</button>
+			</div>
+			<div>
+				<button @click="selectedEmpDetailsSettingsClicked(2,'manage-visit')" class="btn btn-settings"><i class="fa fa-dollar"></i> Manage Cap Per Visit</button>
+			</div>
+			<div>
+				<button @click="selectHealthPartnerView( 'HealthPartnerAccess' )" 
+								class="btn btn-settings">
+								<i class="fa fa-search"></i> Health Partner Access
+				</button>
+			</div>	
+			<div>
+				<button @click="selectedEmpDetailsSettingsClicked(3,'sms-update-notify')" class="btn btn-settings"><i class="fa fa-envelope"></i> Send SMS Update Notification</button>
+			</div>
+		</div>
+
+		<div v-if="showEmpCreditsPlan" class="credits-plans-wrapper">
+			<div>
+				<h1>Credits & Plans</h1>
+			</div>
+			<div>
+				<i @click="openEmpCreditsPlan('cancel')" class="fa fa-times"></i>
+				<div class="credit-plan-container">
+					<div>
+						<h3>Credit Allocation</h3>
+						<div class="credit-type-selector">
+							<button @click="creditAllocationOption('medical')" 
+							v-bind:class="{active: editCreditAllocationOpt == 'medical'}" class="btn-primary">MEDICAL</button>
+							<button @click="creditAllocationOption('wellness')" 
+							v-bind:class="{active: editCreditAllocationOpt == 'wellness'}" class="btn-primary">WELLNESS</button>
+						</div>
+						<div class="credit-details-container">
+							<div class="credit-item-container">
+								<div class="credit-item-detail">
+									<span>
+										<span>S$ </span> 
+										<span>0.00</span>
+									</span>
+									<span>Allocation</span>
+								</div>
+								<div class="credit-item-detail">
+									<span>
+										<span>S$ </span> 
+										<span>0.00</span>
+									</span>
+									<span>Usage</span>
+								</div>
+								<div class="credit-input-wrapper">
+									<input v-if="editCreditAllocationTypeOpt == 'add'" type="number" placeholder="Credits Add*">
+									<input v-if="editCreditAllocationTypeOpt == 'deduct'" type="number" placeholder="Credits Deduct*">
+								</div>
+							</div>
+							<div class="add-deduct-btn-container">
+								<button @click="creditAllocationTypeFunction('add')" v-bind:class="{active: editCreditAllocationTypeOpt == 'add'}" class="btn-primary">ADD</button>
+								<button @click="creditAllocationTypeFunction('deduct')" v-bind:class="{active: editCreditAllocationTypeOpt == 'deduct'}" class="btn-primary">DEDUCT</button>
+							</div>
+						</div>
+						<div class="btn-update-credits">
+							<button class="btn-primary">UPDATE CREDITS</button>
+						</div>
+					</div>
+					<div>
+						<h3>Plan Duration</h3>
+						<div class="plan-type-selector">
+							<label class="container">
+								<span>Standard 1 year</span>
+								<input @click="shortTermRadioBtn('standard-one-year')" type="radio" name="radio">
+								<span class="plan-type-checkmark"></span>
+							</label>
+							<label class="container">
+								<span>Short Term</span>
+								<input @click="shortTermRadioBtn('short-term')" type="radio" name="radio">
+								<span class="plan-type-checkmark"></span>
+							</label>
+							<select v-if="showShortTermSelector">
+								<option>1 month</option>
+								<option>2 months</option>
+							</select>
+						</div>
+						<div class="plan-date-container">
+							<div class="start-date-container">
+								<h4>Start Date:</h4>
+								<div class="start-date-input-wrapper">
+									<v-date-picker
+	                  popoverDirection="bottom"
+	                  v-model="starDateDetails.starDate"
+	                  :input-props='{class: "vDatepicker start-date-input", placeholder: "DD/MM/YYYY", readonly: true, }'
+	                  popover-visibility="focus"
+	                ></v-date-picker>
+								</div>
+							</div>
+							<div class="end-date-container">
+								<h4>End Date: <span>2020-08-02</span></h4> 
+							</div>
+							<div>
+								<button class="btn-primary">UPDATE PLAN</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	import employeeSettings from '@/components/corporates/employee/employeeSettings.vue'
+	export default employeeSettings
+</script>
