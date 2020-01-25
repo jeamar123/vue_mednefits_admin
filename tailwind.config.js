520cb1ec16930071fae79748193ed4aca7cb20bf
Jazer Kemuel Zayas <jaz@Jazers-MacBook-Air.local>
2020-01-25 12:43:12 +0800

    MC-1223-Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
+  important: true,
   theme: {
+    fontFamily: {
+      display: ['Helvetica Medium', 'sans-serif'],
+      body: ['Helvetica Light', 'sans-serif'],
+    },
     extend: {}
   },
   variants: {},
