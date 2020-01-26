cf41c8ddb4e92bee73afbcebffe63dc306bc76dc
Jeamar Libres <jeamarlibres@Jeamars-MacBook-Air.local>
2020-01-27 00:54:57 +0800

    minor updates

--- src/components/corporates.vue
+++ src/components/corporates.vue
@@ -1,5 +1,6 @@
 <script>
 	import axios from 'axios';
+	import moment, { locale } from "moment";
 	import Modal from "../views/modal/Modal.vue";
 
 	var corporates = {
@@ -8,61 +9,246 @@
     },
 		data() {
 			return {
-				startDateDetails: {
-          startDate: undefined,
-        },
-        endDateDetails: {
-        	endDate: undefined,
-        },
+				filterData : {
+					start: null,
+					end: null,
+				},
+				corporate_pagination : {},
+
         isFilterModalShow : false,
         isExportModalShow : false,
-				user_id : null,
 				isSearchShow : false,
+				isPageLimitDropShow : false,
+				allCompanySelected : false,
+
 				search_text : '',
-				allCorporatesSelected : false,
+				searchPropertiesText : '',
+
 				corporate_id_arr : [],
-				corporate_list_arr : [ 
-					{ 
-						corporate_id : 1,
-						company_name : 'StackGeckos',
-						expiry_date : '31/03/2020',
-						active : 'active',
-						total_employee_seat : 110,
-						total_dependent_seat : 42,
-						total_medical_credits : 100,
-						account_type : 'Pro Plan',
-					},
+				corporate_list_arr : [],
+				export_data_header : [
+					'Company Name',
+					'Expiry Date',
+          'HR Account Status',
+          'Total Employee Seats',
+          'Total Dependent Seats',
+          'Total Medical Credits',
+          'Account/Plan Type',
+					'No. of Employees',
+          'No. of Dependents',
+          'Start Date',
+          'Medical Allocated Credits',
+          'Medical Spent Credits',
+          'Wellness Allocated Credits',
+          'Plant Amount',
+          'HR Email Address',
+          'Employee Full Name',
+          'Employee Email Address',
+          'Employee Mobile Number',
+          'Employee Date of Birth',
+          'Employee Status',
+          'Employee Cap Per Visit',
 				],
+				export_data_keys : [
+					'company_name',
+					'end_date',
+					'hr_account_status',
+					'employee_seats',
+					'dependent_seats',
+					'total_medical_credits',
+					'account_type',
+					'no_of_employees',
+          'no_of_dependents',
+          'start_date',
+          'medical_credits_allocated',
+          'medical_spent_credits',
+          'wellness_credits_allocated',
+          'plan_amount',
+          'hr_email',
+          'employee_fullname',
+          'employee_email_address',
+          'mobile',
+          'dob',
+          'employee_status',
+          'employee_cap'
+				],
+				export_data_key_index : [ true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, ],
+
+				page_active : 1,
+				page_limit : 10,
+				pagesToDisplay : 10,
 			}
 		},
 		created() {
+			this.getCompanyList();
+		},
+		computed : {
+			limitPagination(){
+				if( this.corporate_pagination.last_page ){
+					var arr = this.range( this.corporate_pagination.last_page );
+					return arr.slice(this.startIndex(), this.startIndex() + this.pagesToDisplay);
+				}else{
+					return this.range( this.corporate_pagination.last_page );
+				}
+			},
+			searchedExportProperties(){
+				// console.log( this.searchedExportProperties );
+				if( this.searchPropertiesText != '' ){
+					return this.export_data_header.filter((value) => {
+						// console.log( value );
+						return (value.toLowerCase()).startsWith( this.searchPropertiesText.toLowerCase() );
+					})
+				}else{
+					return this.export_data_header;
+				}
+			},
 		},
 		methods: {
-      showSearch(){
-      	this.isSearchShow = true;
-      },
-      hideSearch(){
-      	this.isSearchShow = false;
-      },
-      closeAllModals(){
-      	// if( this.isFilterModalShow == true ){
-      	// 	this.isFilterModalShow = false;
-      	// }
-      	// if( this.isExportModalShow == true ){
-      	// 	this.isExportModalShow = false;
-      	// }
-      },
-      addFilterModal(){
+			range(num) {
+				var arr = [];
+				for (var i = 0; i < num; i++) {
+					arr.push(i);
+				}
+				return arr;
+			},
+			startIndex() {
+				if (this.page_active > ((this.pagesToDisplay / 2) + 1)) {
+					if ((this.page_active + Math.floor(this.pagesToDisplay / 2)) > this.corporate_pagination.last_page) {
+						return this.corporate_pagination.last_page - this.pagesToDisplay + 1;
+					}
+					return this.page_active - Math.floor(this.pagesToDisplay / 2);
+				}
+				return 0;
+			},
+			goToPage( num ){
+				this.page_active = num;
+				this.getCompanyList();
+			},
+			prevPage(){
+				if( this.page_active != 1 ){
+					this.page_active -= 1;
+					this.getCompanyList();
+				}
+			},
+			nextPage(){
+				if( this.page_active != this.corporate_pagination.last_page ){
+					this.page_active += 1;
+					this.getCompanyList();
+				}
+			},
+			setPageLimit( limit ){
+				this.page_limit = limit;
+				this.page_active = 1;
+				this.isPageLimitDropShow = false;
+				this.getCompanyList();
+			},
+			togglePageLimitDrop(){
+				this.isPageLimitDropShow = this.isPageLimitDropShow == true ? false : true;
+			},
+			addFilterModal(){
       	this.isFilterModalShow = this.isFilterModalShow == false ? true : false;
-      },
-      exportModal(){
+			},
+			exportModal(){
       	this.isExportModalShow = this.isExportModalShow == false ? true : false;
+			},
+			allCompanyCheckBox( opt ){
+				this.corporate_id_arr = [];
+				if( opt == false ){
+					this.corporate_list_arr.map((value) => {
+						this.corporate_id_arr.push( value.corporate.customer_id );
+						value.selected = true;
+					});
+				}else{
+					this.corporate_list_arr.map((value) => {
+						value.selected = false;
+					});
+				}
+			},
+			companyCheckBox( list, opt ){
+				if( opt == false ){
+					this.corporate_id_arr.push( list.corporate.customer_id );
+				}else{
+					this.corporate_id_arr.splice( $.inArray( list.corporate.customer_id, this.corporate_id_arr ) , 1);
+				}
+			},
+			goToCompanyDetails( data ){
+				// console.log( data );
+				this.$router.push({ name : 'CorporateMenu' });
+			},
+      closeAllModalsDrop(){
+				// console.log('click outside.');
+				// this.isPageLimitDropShow = false;
+				// this.isFilterModalShow = false;
+				// this.isExportModalShow = false;
       },
-      selectAllCorporatesList( opt ) {
-      	this.allCorporatesSelected = opt;
-      	console.log(this.allCorporatesSelected);
-      	console.log(this.corporate_list_arr);     	
-      },
+			refreshData(){
+				// console.log('so refreshing...');
+			},
+			removeExportKey( index ){
+				this.export_data_key_index[ index ] = false;
+			},
+
+
+
+
+			exportData(){
+				var params = '';
+				this.export_data_keys.map((value, key) => {
+					if( this.export_data_key_index[ key ] == true ){
+						params += ( (value.toLowerCase()).replace(" ", "_") + "=true&" );
+					}
+
+					if( key == this.export_data_keys.length - 1 ){
+						// this.$parent.showLoading();
+						// axios.get( url )
+						// 	.then(res => {
+						// 		console.log( res );
+						// 		this.$parent.hideLoading();
+						// 	})
+						// 	.catch(err => {
+						// 		console.log(err);
+						// 		this.$parent.hideLoading();
+						// 		this.swal('Error!', err, 'error');
+						// 	});
+					}
+				});
+
+				console.log( params );
+			},
+			getCompanyList(){
+				this.$parent.showLoading();
+				this.isFilterModalShow = false;
+				var url = axios.defaults.serverUrl + '/company/corporate?page=' + this.page_active + '&limit' + this.page_limit;
+				if( this.filterData.start != null && this.filterData.end != null ){
+					url += "&start=" + moment( this.filterData.start ).format('YYYY-MM-DD') + "&end=" + moment( this.filterData.end ).format('YYYY-MM-DD');
+				}
+				//  ?page=' + this.page_active + '&limit' + this.page_limit
+				console.log( url );
+				axios.get( url )
+				.then(res => {
+					console.log( res );
+					this.corporate_list_arr = res.data.data;
+					this.corporate_pagination = res.data;
+					this.corporate_pagination.last_page = 15;
+					// console.log(this.corporate_list_arr);
+					// console.log(this.corporate_pagination);
+
+					this.corporate_list_arr.map((value) => {
+						value.selected = false;
+					});
+
+					this.filterData = {
+						start: null,
+						end: null,
+					};
+					this.$parent.hideLoading();
+				})
+				.catch(err => {
+					console.log(err);
+					// this.$parent.hideLoading();
+					this.swal('Error!', err, 'error');
+				});
+			}
     }
 	}
 
--- src/views/Corporates.vue
+++ src/views/Corporates.vue
@@ -46,7 +46,7 @@
         <router-link tag="div" to="/dashboard/corporates/add">
           <img :src="'../assets/img/plus_add.png'">
         </router-link>
-        <div>
+        <div v-on:click="refreshData()">
           <img :src="'../assets/img/refresh.png'">
         </div>
       </div>
@@ -55,12 +55,12 @@
 		<div class="corporate-tbl-list-wrapper">
       <div class="corporate-tbl-list-container">
         <table>
-          <thead v-if="allCorporatesSelected == false">
-            <tr>
+          <thead>
+            <tr v-if="allCompanySelected == false">
               <th>
                 <div class="corporate-list-checkbox-container">
                   <label class="checkbox-input">
-                  <input v-on:click="selectAllCorporatesList( allCorporatesSelected )" v-model="allCorporatesSelected" value="true" type="checkbox">
+                  <input v-on:click="allCompanyCheckBox( allCompanySelected )" v-model="allCompanySelected" value="true" type="checkbox">
                   <span class="checkbox-mark"></span>
                   </label>
                 </div>
@@ -73,15 +73,13 @@
               <th>Total Dependent Seats</th>
               <th>Total Medical Credits</th>
               <th>Account/Plan Type</th>
-              <!-- <th></th> -->
             </tr>
-          </thead>
-          <thead v-if="allCorporatesSelected == true">
-            <tr>
+
+            <tr v-if="allCompanySelected == true">
               <th>
                 <div class="corporate-list-checkbox-container">
                   <label class="checkbox-input">
-                  <input v-model="allCorporatesSelected" value="true" type="checkbox">
+                  <input v-on:click="allCompanyCheckBox( allCompanySelected )" v-model="allCompanySelected" value="true" type="checkbox">
                   <span class="checkbox-mark"></span>
                   </label>
                 </div>
@@ -99,16 +97,11 @@
                   <span>Export</span>
                 </span>
               </th>
-              <th></th>
-              <th></th>
-              <th></th>
-              <th></th>
-              <th></th>
-              <th></th>
+              <th colspan="6"></th>
             </tr>
           </thead>
           <tbody>
-            <tr v-if="allCorporatesSelected == true" class="selected-companies-container">
+            <tr v-if="allCompanySelected == true" class="selected-companies-container">
               <td colspan="9">
                 <span>All 
                   <span>12</span>
@@ -123,50 +116,44 @@
               <td>
                 <div class="corporate-list-checkbox-container">
                   <label class="checkbox-input">
-                  <input type="checkbox">
-                  <span class="checkbox-mark"></span>
+                    <input type="checkbox" v-model="list.selected" v-on:click.self="companyCheckBox( list, list.selected )" value="true">
+                    <span class="checkbox-mark"></span>
                   </label>
                 </div>
               </td>
-              <router-link tag="td" class="corporate-data" :to="{ name: 'CorporateMenu' }">
-                <span>{{ list.corporate_id }}</span>
-              </router-link>
- 
-              <router-link tag="td" class="corporate-data" :to="{ name: 'CorporateMenu' }">
-                <span>{{ list.company_name }}</span>
-              </router-link>
-            
-              <router-link tag="td" class="corporate-data" :to="{ name: 'CorporateMenu' }">
+              <td v-on:click="goToCompanyDetails( list )">
+                <span>{{ list.corporate.customer_id }}</span>
+              </td>
+              <td v-on:click="goToCompanyDetails( list )">
+                <span>{{ list.corporate.company_name }}</span>
+              </td>
+              <td v-on:click="goToCompanyDetails( list )">
                 <span>{{ list.expiry_date }}</span>
-              </router-link>
-            
-              <router-link tag="td" class="corporate-data" :to="{ name: 'CorporateMenu' }">
-                <span>{{ list.active }}</span>
-                 <!-- <span>Deactivated</span> -->
-              </router-link>
-            
-              <router-link tag="td" class="corporate-data" :to="{ name: 'CorporateMenu' }">
+              </td>
+              <td v-on:click="goToCompanyDetails( list )">
+                <span>{{ list.hr.active == 1 ? 'Active' : 'Deactivated' }}</span>
+              </td>
+              <td v-on:click="goToCompanyDetails( list )">
                 <span>{{ list.total_employee_seat }}</span>
-              </router-link>
-            
-              <router-link tag="td" class="corporate-data" :to="{ name: 'CorporateMenu' }">
+              </td>
+              <td v-on:click="goToCompanyDetails( list )">
                 <span>{{ list.total_dependent_seat }}</span>
-              </router-link>
-            
-              <router-link tag="td" class="corporate-data" :to="{ name: 'CorporateMenu' }">
+              </td>
+              <td v-on:click="goToCompanyDetails( list )">
                 <span>{{ list.total_medical_credits }}</span>
-              </router-link>
-            
-              <router-link tag="td" class="corporate-data" :to="{ name: 'CorporateMenu' }">
-                <span>{{ list.account_type }}</span>
-                <!-- <span>Insurance Bundle</span>
-                <span>Trial Plan</span>
+              </td>
+              <td v-on:click="goToCompanyDetails( list )">
+                <span v-show="list.employees.account_type == 'stand_alone_plan'">Pro Plan</span>
+                <span v-show="list.employees.account_type == 'insurance_bundle'">Insurance Bundle</span>
+                <span v-show="list.employees.account_type == 'trial_plan'">Trial Plan</span>
+                <span v-show="list.employees.account_type == 'lite_plan'">Lite Plan</span>
+                <span v-show="list.employees.account_type == 'super_pro_plan'">Super Pro Plan</span>
+                <span v-show="list.employees.account_type == 'enterprise_plan'">Enterprise Plan</span>
+                <!-- <span>Trial Plan</span>
                 <span>Lite Plan</span>
                 <span>Super Pro Plan</span>
                 <span>Enterprise Plan</span> -->
-              </router-link>
-              
-              
+              </td>
             </tr>
           </tbody>
         </table>
@@ -176,35 +163,35 @@
     <div class="corporate-pagination">
       <div class="page-wrapper">
         <div class="page-scroll-container">
-          <span class="prev-next-container">  
+          <span class="prev-next-container" v-on:click="prevPage()">  
             <i class="fa fa-angle-left"></i> 
             <span>Prev</span>
           </span>
-          <div>
-            <span class="page-num active">1</span>
-            <span class="page-num">2</span>
-            <span class="page-num">3</span>
-            <span class="page-num">4</span>
-            <span class="page-num">5</span>
-            ..
-            <span class="page-num">11</span>
+          <div class="pages-list">
+            <span class="page-num" v-show="page_active > 6" v-on:click="goToPage( 1 )">1</span>
+            <span v-show="page_active > 6" style="margin-left: -10px;">...</span>
+
+            <span v-for="(list, index) of limitPagination" class="page-num" v-bind:class="{'active' : list + 1 == page_active}" v-on:click="goToPage( list + 1 )">{{ list + 1 }}</span>
+            
+            <span v-show="corporate_pagination.last_page > 10 && page_active < ( corporate_pagination.last_page - 5 )" style="margin-right: -5px;">...</span>
+            <span class="page-num" v-show="corporate_pagination.last_page > 10 && page_active < ( corporate_pagination.last_page - 5 )" v-on:click="goToPage( corporate_pagination.last_page )">{{ corporate_pagination.last_page }}</span>
           </div>
-          <span class="prev-next-container">  
+          <span class="prev-next-container" v-on:click="nextPage()">  
             <span>Next</span>
             <i class="fa fa-angle-right"></i> 
           </span>
         </div>
       </div>
       <div class="corporate-list-per-page">
-        <span><span>10</span> per page</span>
-        <span>  
+        <span v-on:click="togglePageLimitDrop()"><span>{{ page_limit }}</span> per page</span>
+        <span v-on:click="togglePageLimitDrop()">  
           <i class="fa fa-caret-down"></i>
         </span>
-        <div class="opened-per-page-scroll" v-if="false">
-          <span>10 per page</span>
-          <span>25 per page</span>
-          <span>50 per page</span>
-          <span>100 per page</span>
+        <div class="opened-per-page-scroll" v-if="isPageLimitDropShow">
+          <span v-on:click="setPageLimit( 10 )">10 per page</span>
+          <span v-on:click="setPageLimit( 25)">25 per page</span>
+          <span v-on:click="setPageLimit( 50 )">50 per page</span>
+          <span v-on:click="setPageLimit( 100 )">100 per page</span>
         </div>
       </div>
     </div>
@@ -282,9 +269,9 @@
 		</div> -->
 
 
-  <div v-click-outside="closeAllModals">
+  <div v-click-outside="closeAllModalsDrop">
     <!-- Add Filter -->
-    <Modal v-if="isFilterModalShow" class="add-filter-wrapper">
+    <Modal v-if="isFilterModalShow" class="add-filter-wrapper" >
       <div slot="header">
         <h3>Period of expiry</h3>
         <img v-on:click="addFilterModal()" class="cancel-icon" :src="'../assets/img/cancel.png'"></i>
@@ -296,7 +283,7 @@
             <div class="date-container">
               <v-date-picker
                 popoverDirection="bottom"
-                v-model="startDateDetails.null"
+                v-model="filterData.start"
                 :input-props='{class: "vDatepicker", placeholder: "DD/MM/YYYY", readonly: true, }'
                 popover-visibility="focus"
               ></v-date-picker>
@@ -308,7 +295,7 @@
             <div class="date-container">
               <v-date-picker
                 popoverDirection="bottom"
-                v-model="endDateDetails.null"
+                v-model="filterData.end"
                 :input-props='{class: "vDatepicker", placeholder: "DD/MM/YYYY", readonly: true, }'
                 popover-visibility="focus"
               ></v-date-picker>
@@ -318,7 +305,7 @@
         </div>
       </div>
       <div slot="footer">
-        <button class="btn-primary">Apply Filter</button>
+        <button class="btn-primary" v-on:click="getCompanyList()">Apply Filter</button>
       </div>
     </Modal>
 
@@ -332,15 +319,15 @@
         <div class="dl-corp-content-wrapper">
           <div class="properties-container">
             <div class="properties-search-container">
-              <input type="text" placeholder="Search properties...">
+              <input type="text" placeholder="Search properties..." v-model="searchPropertiesText">
               <i class="fa fa-search"></i>
             </div>
             <div class="properties-title">Properties</div>
-            <div class="column-check-box">
+            <div v-for="( list, index ) in searchedExportProperties" class="column-check-box">
               <div class="corporate-list-checkbox-container">
                 <label class="checkbox-input">
-                  <span class="checkbox-label">Company Name</span>
-                  <input type="checkbox">
+                  <span class="checkbox-label">{{ list }}</span>
+                  <input type="checkbox" v-model="export_data_key_index[index]" value="true">
                   <span class="checkbox-mark"></span>
                 </label>
               </div>
@@ -348,15 +335,15 @@
           </div>
           <div class="selected-columns-container">
             <div class="data-column-header-title">SELECTED COLUMNS<span>(1)</span></div>
-            <div class="selected-col-box-container">
-              <span>Company Name</span>
-              <i class="fa fa-times"></i>
+            <div v-show="export_data_key_index[ index ] == true" class="selected-col-box-container" v-for=" (list, index) in export_data_header">
+              <span>{{ list }}</span>
+              <i class="fa fa-times" v-on:click="removeExportKey( index )"></i>
             </div>
           </div>
         </div>
       </div>
       <div slot="footer">
-        <button class="btn-primary">Download</button>
+        <button class="btn-primary" v-on:click="exportData()">Download</button>
       </div>
     </Modal>
   </div>
