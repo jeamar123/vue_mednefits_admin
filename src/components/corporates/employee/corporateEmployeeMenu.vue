504fb998a4aac996d564aac999894cd7c92c7234
Jazer Kemuel Zayas <jaz@Jazers-MacBook-Air.local>
2020-01-30 19:44:30 +0800

    MC-1228- code refractor

--- src/components/corporates/employee/corporateEmployeeMenu.vue
+++ src/components/corporates/employee/corporateEmployeeMenu.vue
@@ -1,30 +1,34 @@
 <script>
-  let corporateEmployeeMenu = {
-    data() {
-      return {
-        data : null,
-        sideBar: {
-        trigger: false,
-      }
-      };
-    },
-    created(){
-      console.log( this.$route );
-    },
-    methods: {
-      toggleSideInfoBar(opt) {
-        //sidebar information in mobile view
-        this.sideBar.trigger = !this.sideBar.trigger;
-      },
-      showEmpSelectorInfo(  page ) {
-        this.$router.push({ name : page });
-      },
-    }
-  }
-  
-  export default corporateEmployeeMenu
+let corporateEmployeeMenu = {
+	props: {
+		customer_id: [String, Number],
+	},
+	data() {
+		return {
+			data: null,
+			sideBar: {
+				trigger: false,
+			}
+		};
+	},
+	created() {
+		console.log(this.customer_id);
+		console.log(this.$route);
+	},
+	methods: {
+		toggleSideInfoBar(opt) {
+			//sidebar information in mobile view
+			this.sideBar.trigger = !this.sideBar.trigger;
+		},
+		showEmpSelectorInfo(page) {
+			this.$router.push({ name: page });
+		},
+	}
+}
+
+export default corporateEmployeeMenu
 </script>
 
 <style lang="scss" scoped>
-	@import "./src/assets/css/corporateEmployee.scss";
+@import "./src/assets/css/corporateEmployee.scss";
 </style>
