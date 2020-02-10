9f4e809df62e484c329e175353abfead6ecd769e
Jeamar Libres <jeamarlibres@Jeamars-MacBook-Air.local>
2020-02-10 19:33:26 +0800

    MC-1303-integrate health partners

--- src/components/corporates/healthPartnerAccess.vue
+++ src/components/corporates/healthPartnerAccess.vue
@@ -13,11 +13,15 @@ import moment, { locale } from "moment";
         block_clinic_opt: 'type',
         block_clinic_region: 'sgd',
         block_clinic_search: null,
+        block_active_page: 1,
+        block_page_limit: 10,
 
         open_clinic_list: [],
         open_clinic_opt: 'type',
         open_clinic_region: 'sgd',
         open_clinic_search: null,
+        open_active_page: 1,
+        open_page_limit: 10,
       };
     },
     created(){
@@ -28,7 +32,7 @@ import moment, { locale } from "moment";
       getClinicList(){
         // 
 
-        axios.get( axios.defaults.serverUrl + '/company/clinic?token=' + localStorage.getItem('vue_admin_session') + '&corporate_id=' + this.id + '&region[]=sgd&region[]=sgd' )
+        axios.get( axios.defaults.serverUrl + '/company/clinic?token=' + localStorage.getItem('vue_admin_session') + '&corporate_id=' + this.id + '&region[]=' + this.open_clinic_region + '&region[]=' + this.block_clinic_region + '&page=1&limit=10' )
           .then(res => {
             console.log( res );
             this.block_clinic_list = res.data.clinic_block;
