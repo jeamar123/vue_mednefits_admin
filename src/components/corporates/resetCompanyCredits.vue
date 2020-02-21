0119461bd6a8dd85744d8bf210b6b03f0be40ca0
Noelou Nagac <noelounagac@Noelous-MacBook-Air.local>
2020-02-21 15:34:36 +0800

    feature-MC-1317

---
+++ src/components/corporates/resetCompanyCredits.vue
@@ -0,0 +1,33 @@
+<script>
+  let resetCompanyCredits = {
+    data() {
+      return {
+        // empSelectorActive: {
+        //   value: 0,
+        //   text: ""
+        // },
+        // corporateViewStatus : 'CorporateDetails',
+        formats: {
+          input: ["DD/MM/YYYY"],
+          data: ["DD/MM/YYYY"]
+        },
+        global_resetEffectiveDate:  undefined,
+      };
+    },
+    created(){
+      // this.corporateViewStatus = this.$route.name;
+    },
+    methods: {
+      // selectCorporateView( opt ){
+      //   this.corporateViewStatus = opt;
+      //   this.$router.push( { name : opt } );
+      // }
+    }
+  }
+  
+  export default resetCompanyCredits
+</script>
+
+<style lang="scss" scoped>
+	// @import "./src/assets/css/corporates.scss";
+</style>
---
+++ src/views/corporates/ResetCompanyCredits.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="reset-company-credits-wrapper">
+    <div class="dp=flex">
+		  <h1>Reset Company Credits</h1>
+      <div class="flex-1">
+        <img :src="'../assets/img/download_emp.png'">
+        <button>DOWNLOAD EMPLOYEE LIST</button>
+      </div>
+    </div>
+    <div class="reset-company-body">
+      <div>
+        <div>
+          <label>Effective Date*</label>
+           <div class="entitlement-input-label">
+              <div class="entitlement-date-picker">
+                <v-date-picker
+                  popoverDirection="bottom"
+                  v-model="global_resetEffectiveDate"
+                  :formats='formats'
+                  :input-props='{class: "vDatepicker", placeholder: "DD/MM/YYYY", readonly: true, }'
+                  popover-visibility="focus"
+                ></v-date-picker>
+                <i class="fa fa-calendar-o"></i>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="reset-company-footer">
+
+    </div>
+	</div>
+</template>
+
+<script>
+	import resetCompanyCredits from '@/components/corporates/resetCompanyCredits'
+	export default resetCompanyCredits
+</script>
