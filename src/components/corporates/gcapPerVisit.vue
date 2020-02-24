7960c249b22c2afc799f81cf78cb9e80f4d4d232
Noelou Nagac <noelounagac@Noelous-MacBook-Air.local>
2020-02-24 12:03:58 +0800

    feature-MC-1324

--- src/components/corporates/gcapPerVisit.vue
+++ src/components/corporates/gcapPerVisit.vue
@@ -1,9 +1,15 @@
 <script>
+  import Modal from "../../views/modal/Modal.vue";
+
   let gcapPerVisit = {
+    components: {
+      Modal
+    },
     data() {
       return {
         global_showInput: false,
-        global_showText: true,
+        global_showText:  true,
+        global_selectedIndex: 0,
         global_getGpData: [
           {
             id: 0,
@@ -16,21 +22,22 @@
             amount: 250
           }
         ],
+        global_showFileUpload:  false,
       };
     },
     created(){
       // this.corporateViewStatus = this.$route.name;
     },
     methods: {
-      ___editTable( data ) { 
-
-          
-
-        _.forEach( [data], function(value) {
-          console.log(value);
-          
-        });
+      ___editTable( data, index ) { 
+        // console.log(index);
+        this.global_showText = false;
+        this.global_showInput  = true;
+        // this.global_selectedIndex = index;
       },
+      ___fileUploadModal() {
+        this.global_showFileUpload = this.global_showFileUpload == false ? true : false;
+      }
     }
   }
   
@@ -39,5 +46,5 @@
 
 <style lang="scss" scoped>
 	/*@import "./src/assets/css/corporates.scss";*/
-  @import "./src/assets/css/corporateSettings.scss";
+  // @import "./src/assets/css/corporateSettings.scss";
 </style>
