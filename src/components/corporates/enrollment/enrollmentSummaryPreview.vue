977f48b51f13aa136e09f8099cbd76d50b532d03
Jeamar Libres <jeamarlibres@Jeamars-MacBook-Air.local>
2020-02-25 15:23:32 +0800

    MC-1307-enrollment-method

--- src/components/corporates/enrollment/enrollmentSummaryPreview.vue
+++ src/components/corporates/enrollment/enrollmentSummaryPreview.vue
@@ -1,13 +1,34 @@
 <script>
+import Modal from "../../../views/modal/Modal.vue";
+import jobList from '../../../assets/json/job.json';
+
   let enrollmentSummaryPreview = {
+    components: {
+      Modal
+    },
     data() {
       return {
+        formats: {
+          input: ["DD/MM/YYYY"],
+          data: ["DD/MM/YYYY"]
+        },
+        global_isSummaryEditModalShow: false,
+        global_selectedEmployee: {
+          dependents: [
+            {
+              dob: new Date(),
+            }
+          ],
+        },
+        jobList: jobList,
       };
     },
     created(){
     },
     methods: {
-     
+      _toggleSummaryEditModal_(){
+        this.global_isSummaryEditModalShow = this.global_isSummaryEditModalShow ? false : true;
+      }
     }
   }
   
--- src/views/corporates/enrollment/EnrollmentSummaryPreview.vue
+++ src/views/corporates/enrollment/EnrollmentSummaryPreview.vue
@@ -27,11 +27,16 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
+          <tr class="has-error">
             <td>
               <input type="checkbox">
             </td>
-            <td>A</td>
+            <td>
+              <div class="fullname-box">
+                <div class="fullname-text">Wei Xian Jun Xi Kuan Yen Li</div>
+                <div class="edit-icon" v-on:click="_toggleSummaryEditModal_()"><i class="fa fa-pencil"></i></div>
+              </div>
+            </td>
             <td>A</td>
             <td>A</td>
             <td>A</td>
@@ -86,6 +91,132 @@
         <button class="btn-gray-with-shadow" disabled="true">ENROLL</button>
       </div>
     </div>
+
+    <div>
+			<Modal v-if="global_isSummaryEditModalShow" class="summary-edit-employee-modal">
+				<div slot="header">
+					<h1>Edit Employee Details</h1>
+				</div>
+				<div slot="body">
+					<div class="summary-edit-emp-form">
+            <div class="form-box">
+              <label>Full Name*</label>
+              <input type="text">
+            </div>
+            <div class="form-box">
+              <label>Date of Birth</label>
+              <div class="date-container vDatepicker">
+                <v-date-picker 
+                  popoverDirection="bottom" 
+                  v-model="global_selectedEmployee.dob"
+                  :input-props='{class: "vDatepicker", placeholder: "DD/MM/YYYY", readonly: true, }'
+                  popover-visibility="focus" 
+                  :formats='formats'></v-date-picker>
+                <i class="fa fa-caret-down"></i>
+              </div>
+            </div>
+            <div class="form-box">
+              <label>Work Email</label>
+              <input type="text">
+            </div>
+            <div class="form-box">
+              <label>Mobile Area Code</label>
+              <select>
+                <option value=""></option>
+                <option value="+65">(SG) +65</option>
+                <option value="+63">(PH) +63</option>
+                <option value="+60">(MY) +60</option>
+              </select>
+              <i class="fa fa-caret-down"></i>
+            </div>
+            <div class="form-box">
+              <label>Mobile</label>
+              <input type="text">
+            </div>
+            <div class="form-box">
+              <label>Job Title</label>
+              <select>
+                <option value=""></option>
+                <option :value="jobs.job_title" v-for="jobs in jobList" :key="jobs.index">
+                  {{jobs.job_title}}
+                </option>
+              </select>
+              <i class="fa fa-caret-down"></i>
+            </div>
+            <div class="form-box">
+              <label>Medical Credits</label>
+              <input type="text">
+            </div>
+            <div class="form-box">
+              <label>Wellness Credits</label>
+              <input type="text">
+            </div>
+            <div class="form-box">
+              <label>Start Date</label>
+              <div class="date-container vDatepicker">
+                <v-date-picker 
+                  popoverDirection="bottom" 
+                  v-model="global_selectedEmployee.start_date"
+                  :input-props='{class: "vDatepicker", placeholder: "DD/MM/YYYY", readonly: true, }'
+                  popover-visibility="focus" 
+                  :formats='formats'></v-date-picker>
+                <i class="fa fa-caret-down"></i>
+              </div>
+            </div>
+
+            <div v-for="(list,index) in global_selectedEmployee.dependents" class="dependent-form">
+              <p>Dependent {{index+1}}</p>
+              <div class="form-box">
+                <label>Full Name*</label>
+                <input type="text">
+              </div>
+              <div class="form-box">
+                <label>Date of Birth</label>
+                <div class="date-container vDatepicker">
+                  <v-date-picker 
+                    popoverDirection="bottom" 
+                    v-model="global_selectedEmployee.dependents[index].dob"
+                    :input-props='{class: "vDatepicker", placeholder: "DD/MM/YYYY", readonly: true, }'
+                    popover-visibility="focus" 
+                    :formats='formats'></v-date-picker>
+                  <i class="fa fa-caret-down"></i>
+                </div>
+              </div>
+              <div class="form-box">
+                <label>Relationship</label>
+                <select>
+                  <option value=""></option>
+                  <option value="spouse">Spouse</option>
+                  <option value="child">Child</option>
+                  <option value="parent">Parent</option>
+                  <option value="sibling">Sibling</option>
+                  <option value="family">Family</option>
+                </select>
+                <i class="fa fa-caret-down"></i>
+              </div>
+              <div class="form-box">
+                <label>Start Date</label>
+                <div class="date-container vDatepicker">
+                  <v-date-picker 
+                    popoverDirection="top" 
+                    v-model="global_selectedEmployee.start_date"
+                    :input-props='{class: "vDatepicker", placeholder: "DD/MM/YYYY", readonly: true, }'
+                    popover-visibility="focus" 
+                    :formats='formats'></v-date-picker>
+                  <i class="fa fa-caret-down"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+				</div>
+				<div slot="footer">
+					<button v-on:click="_toggleSummaryEditModal_()" class="btn-close">CANCEL</button>
+					<button v-on:click="_toggleSummaryEditModal_()" class="btn-delete">DELETE</button>
+	  			<button class="btn-primary settings-btn-submit" v-on:click="submitInNetwork( inNetwork_data )">Update</button>
+				</div>
+			</Modal>
+
+		</div>
 	</div>
 </template>
 
