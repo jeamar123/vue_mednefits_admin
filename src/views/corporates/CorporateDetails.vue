8a9fd6c92fb20ca717803f1d6ff980928361066a
Jeamar Libres <jeamarlibres@Jeamars-MacBook-Air.local>
2020-03-03 13:26:04 +0800

    MC-1438-business details

--- src/views/corporates/CorporateDetails.vue
+++ src/views/corporates/CorporateDetails.vue
@@ -1,10 +1,22 @@
 <template>
 	<div class="corporate-details-wrapper">
     <div class="tab-container">
-      <div class="tab active">Business Details</div>
-      <div class="tab">Plan</div>
-      <div class="tab">Credentials</div>
-      <div class="tab">Spending Invoice</div>
+      <div class="tab" v-bind:class="{ 'active' : $route.name == 'BusinessDetails' }">Business Details</div>
+      <div class="tab" v-bind:class="{ 'active' : $route.name == 'CorporatePlans' }">Plan</div>
+      <div class="tab" v-bind:class="{ 'active' : $route.name == 'CorporateCredentials' }">Credentials</div>
+      <div class="tab" v-bind:class="{ 'active' : $route.name == 'CorporateSpendingInvoice' }">Spending Invoice</div>
+    </div>
+
+    <div class="xs-tab-container">
+      <div class="select-tab-box">
+        <select class="tab-select">
+          <option value="EmployeeInformation">Business Details</option>		
+          <option value="DependentInformation">Plan</option>		
+          <option value="ClaimSubmission">Credentials</option>		
+          <option value="Entitlement">Spending Invoice</option>			
+        </select>
+        <i class="fa fa-caret-down"></i>
+      </div>
     </div>
     <router-view name="childTab"></router-view>
 	</div>
